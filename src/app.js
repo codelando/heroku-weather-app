87e18709c3461782529fdfda1fedbc070cc48ba4
Ezequiel Cortés <codelando@gmail.com>
2019-07-24 23:50:56 -0300

    First commit

---
+++ src/app.js
@@ -0,0 +1,100 @@
+const path = require('path');
+const express = require('express');
+const hbs = require('hbs');
+const forecast = require('./utils/forecast.js');
+const geocode = require('./utils/geocode.js');
+
+
+// console.log(__dirname);
+// console.log(__filename);
+// console.log(path.join(__dirname, '../public'));
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Express paths config.
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
+
+// Set Handelbars view engine and views location.
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+// Use the route defined for static content, 
+// This will override any matching route after it.
+app.use(express.static(publicDirectoryPath));
+
+
+// Routes get evaluated in order
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Lando'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About',
+        name: 'Lando'
+    });
+});
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help',
+        name: 'Lando',
+        message: 'This is a help text.'
+    });
+});
+
+app.get('/weather', (req, res) => {
+    if(!req.query.address) {
+        return res.send({ 
+            error: 'You must provide an address'
+        });
+    }
+
+    geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+        if (error) {
+            return res.send({ error });
+        }
+        
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send({ error });
+            }
+    
+            res.send({
+                address: req.query.address,
+                location,
+                forecast: forecastData
+            });
+        })
+    });
+
+
+});
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: 'Error',
+        name: 'Lando',
+        message: 'Help article not found.'
+    })
+});
+
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Lando',
+        message: 'Page not found.'
+    })
+});
+
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}.`);
+});
